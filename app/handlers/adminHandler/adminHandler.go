e0ae0253f64ee9f677d8f8e74a782d7802c8fab4
gokulmaxi <gokulakams@gmail.com>
2023-02-13 20:33:16 +0530

    added two new apis to list users

--- app/handlers/adminHandler/adminHandler.go
+++ app/handlers/adminHandler/adminHandler.go
@@ -6,8 +6,12 @@ import (
 	"app/utilities"
 	"context"
 	"encoding/json"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func signup(c *fiber.Ctx) error {
@@ -21,7 +25,64 @@ func signup(c *fiber.Ctx) error {
 	}
 	return c.Send(utilities.MsgJson(utilities.Success))
 }
-
+func userList(c *fiber.Ctx) error {
+	coll := database.Instance.Db.Collection("users")
+	matchStage := bson.D{
+		{"$match", bson.D{{"user_type", "user"}}}}
+	projectStage := bson.D{{
+		"$project", bson.D{
+			{"name", 1},
+			{"data", 1},
+			{"email", 1},
+			{"accound_status", bson.D{{"$not", "$account_deactivated"}}},
+		},
+	}}
+	cursor, err := coll.Aggregate(context.TODO(), mongo.Pipeline{matchStage, projectStage})
+	if err != nil {
+		fmt.Println(err)
+		return c.Send(utilities.MsgJson(utilities.Failure))
+	}
+	var data []bson.M
+	if err = cursor.All(context.TODO(), &data); err != nil {
+		fmt.Println(err)
+		return c.Send(utilities.MsgJson(utilities.Failure))
+	}
+	jsondata, err := json.Marshal(data)
+	if data == nil {
+		return c.Send(utilities.MsgJson(utilities.NoData))
+	}
+	return c.Send(jsondata)
+}
+func adminList(c *fiber.Ctx) error {
+	coll := database.Instance.Db.Collection("users")
+	matchStage := bson.D{
+		{"$match", bson.D{{"user_type", "admin"}}}}
+	projectStage := bson.D{{
+		"$project", bson.D{
+			{"name", 1},
+			// {"data", 1},
+			{"email", 1},
+			{"accound_status", bson.D{{"$not", "$account_deactivated"}}},
+		},
+	}}
+	cursor, err := coll.Aggregate(context.TODO(), mongo.Pipeline{matchStage, projectStage})
+	if err != nil {
+		fmt.Println(err)
+		return c.Send(utilities.MsgJson(utilities.Failure))
+	}
+	var data []bson.M
+	if err = cursor.All(context.TODO(), &data); err != nil {
+		fmt.Println(err)
+		return c.Send(utilities.MsgJson(utilities.Failure))
+	}
+	jsondata, err := json.Marshal(data)
+	if data == nil {
+		return c.Send(utilities.MsgJson(utilities.NoData))
+	}
+	return c.Send(jsondata)
+}
 func Register(_route fiber.Router) {
 	_route.Post("/create", signup)
+	_route.Get("/user/list", userList)
+	_route.Get("/list", adminList)
 }
